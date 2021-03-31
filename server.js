9be1c595eba8c5e639aa632b6e991862e27d64a8
Jacob Holmer <yacub90@gmail.com>
2021-03-30 18:16:00 -0700

    debugging

--- server.js
+++ server.js
@@ -29,22 +29,25 @@ const PORT = process.env.PORT || 3002;
 const jacob = new User ({
   email: 'yacub90@gmail.com',
   books: [
-    [{name: 'The Alchemist'}, {description: 'Book about a boy who finds his true calling.'}, {status: 'read'}], 
-    [{name: 'The Way Of The Peaceful Warrior'}, {description: 'A story about living in the present moment.'}, {status: 'read'}],
-    [{name: 'The Bhagavad Gita'}, {description: 'Hope you can read sanskrit'}, {status: 'read'}]
+    {name: 'The Alchemist',description: 'Book about a boy who finds his true calling.',status: 'read'}, 
+    {name: 'The Way Of The Peaceful Warrior', description: 'A story about living in the present moment.', status: 'read'},
+    {name: 'The Bhagavad Gita', description: 'Hope you can read sanskrit', status: 'read'}
   ]
 })
 jacob.save();
+console.log(jacob.books);
+
 
 const simone = new User ({
   email: 'team.steele@gmail.com',
   books: [
-    [{name: 'Holes'}, {description: 'A book about diggin holes and findin freedom'}, {status: 'read'}],
-    [{name: 'Gossip Girl'}, {description: 'Hey uppereast siders, this is about the scandals of the elite of Manhattan.'}, {status: 'read'}],
-    [{name: 'Demon Slayer Manga'}, {description: 'This is about a Demon Slayer'}, {status: 'read'}]
+    {name: 'Holes', description: 'A book about diggin holes and findin freedom', status: 'read'},
+    {name: 'Gossip Girl', description: 'Hey uppereast siders, this is about the scandals of the elite of Manhattan.', status: 'read'},
+    {name: 'Demon Slayer Manga', description: 'This is about a Demon Slayer', status: 'read'}
   ]
 })
 simone.save();
+console.log(simone.books);
 
 
 // routes
@@ -56,13 +59,13 @@ app.get('/books', getAllBooks)
 
 async function getAllBooks(request, response){
   const email = request.query.email;
-  console.log({email})
+  console.log('jk',{email})
   await User.find({email: email}, function (err, items) {
     if (err) return console.error(err);
-    console.log(items, items[0])
+    console.log(items[0])
     response.status(200).send(items[0].books);
   })
+  console.log(email)
 }
-
 // turn on server
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
